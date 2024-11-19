15d3f86ea241a0f30037e4cd53cc1487e076c331
Erilocshada <erilleo23@gmail.com>
2024-11-19 11:56:51 +0700

    Percobaan2 Pertanyaan5

--- Kafe20Percobaan2.java
+++ Kafe20Percobaan2.java
@@ -1,7 +1,7 @@
 
 
 public class Kafe20Percobaan2 {
-    public static void menu(String namaPelanggan, boolean isMember) {
+    public static void menu(String namaPelanggan, boolean isMember, String kodePromo) {
         System.out.println("Selamat Datang, "+namaPelanggan+" !");
         if (isMember) {
             System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap Pembelian!");
@@ -14,9 +14,18 @@ public class Kafe20Percobaan2 {
         System.out.println("5. Roti Bakae - RP 10,000");
         System.out.println("6. Mie Goreng - RP 18,000");
         System.out.println("=========================");
+        if (kodePromo.equalsIgnoreCase("DISKON50%")) {
+            System.out.println("Selamat! Anda mendapatkan diskon 50% untuk setiap pembelian.");
+        } else if (kodePromo.equalsIgnoreCase("DISKON30%")) {
+            System.out.println("Selamat! Anda mendapatkan diskon 30% untuk setiap pembelian.");
+        }else if (!kodePromo.isEmpty()) {
+            System.out.println("Kode Promo \""+kodePromo+"\" Tidak Valid");
+        }else {
+            System.out.println("Tidak ada Promo yang digunakan.");
+        }
         System.out.println("Silahkan Pilih Menu yang anda inginkan.");
     }
     public static void main(String[] args) {
-        menu("Andi",  true);
+        menu("Andi",  true, "DISKON20%");
     }
 }
